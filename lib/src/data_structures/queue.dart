b70d9a0450fc7a53fb64e26ba6ccf527b7159294
Samuel Vargas <samvargasca@unal.edu.co>
2023-09-27 18:47:45 -0500

    Creación de la estructura cola

---
+++ lib/src/data_structures/queue.dart
@@ -0,0 +1,44 @@
+class Queue<T> {
+  late int _front, _rear, _count;
+  late List<T?> _queue;
+
+  //Constructores
+  Queue(int n) {
+    _front = 0;
+    _rear = 0;
+    _count = 0;
+    _queue = List<T?>.filled(n, null);
+  }
+
+  // Métodos
+  bool isEmpty() => _count <= 0;
+
+  bool isFull() => _count >= _queue.length;
+
+  void enqueue(T item) {
+    if (isFull()) {
+      throw Exception("La cola está llena");
+    }
+    _queue[_rear] = item;
+    _rear = (_rear + 1) % _queue.length;
+    _count++;
+  }
+
+  T dequeue() {
+    if (isEmpty()) {
+      throw Exception("La cola está vacía");
+    }
+    T item = _queue[_front] as T;
+    _front = (_front + 1) % _queue.length;
+    _count--;
+    return item;
+  }
+
+  T peek() {
+    if (isEmpty()) {
+      throw Exception("La cola está vacía");
+    }
+    T item = _queue[_front] as T;
+    return item;
+  }
+}
