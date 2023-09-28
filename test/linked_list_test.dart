a3d4d50e88ea2aa65e4f3f05ea58ce01586ff644
Samuel Vargas <samvargasca@unal.edu.co>
2023-09-27 21:19:52 -0500

    Tests adicionales para linked list

--- test/linked_list_test.dart
+++ test/linked_list_test.dart
@@ -18,10 +18,33 @@ void main() {
       expect(linkedList.popFront(), 1);
     });
 
+    test("La lista debe poder añadir elementos al final", () {
+      LinkedList linkedList = LinkedList();
+      linkedList.pushBack(1);
+      linkedList.pushBack(2);
+      linkedList.pushBack(3);
+      linkedList.pushBack(4);
+
+      expect(linkedList.popBack(), 4);
+      expect(linkedList.popBack(), 3);
+      expect(linkedList.popBack(), 2);
+      expect(linkedList.popBack(), 1);
+    });
+
     test("La lista no debería dejar sacar elementos cuando está vacía", () {
       LinkedList linkedList = LinkedList();
       expect(() => linkedList.popBack(), throwsException);
       expect(() => linkedList.popFront(), throwsException);
+    });
+
+    test("addAfter debe agregar elementos después del nodo especificado", () {
+      LinkedList<int> lista = LinkedList<int>();
+      lista.pushBack(1);
+      lista.pushBack(2);
+      lista.addAfter(lista.head!, 3);
+      expect(lista.popFront(), equals(1));
+      expect(lista.popFront(), equals(3));
+      expect(lista.popFront(), equals(2));
     });
   });
 }
