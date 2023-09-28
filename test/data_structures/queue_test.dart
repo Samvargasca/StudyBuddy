058cca64dfaeca870601995c2825da34e1855a26
Samuel Vargas <samvargasca@unal.edu.co>
2023-09-28 09:58:18 -0500

    Creación de tests para colas

---
+++ test/data_structures/queue_test.dart
@@ -0,0 +1,35 @@
+import 'package:test/test.dart';
+import 'package:study_buddy/src/data_structures/queue.dart';
+
+void main() {
+  group("Cola", () {
+    test("La cola no deja sacar elementos cuando está vacía", () {
+      Queue queue = Queue<int>(3);
+      expect(() => queue.dequeue(), throwsException);
+    });
+
+    test("La cola no deja entrever elementos cuando está vacía", () {
+      Queue queue = Queue<int>(3);
+      expect(() => queue.peek(), throwsException);
+    });
+    test("La cola no deja insertar elementos cuando está llena", () {
+      Queue queue = Queue<int>(3);
+      queue.enqueue(1);
+      queue.enqueue(2);
+      queue.enqueue(3);
+      expect(() => queue.enqueue(4), throwsException);
+    });
+
+    test("La cola funciona correctamente (FIFO).", () {
+      Queue queue = Queue<int>(4);
+      queue.enqueue(1);
+      queue.enqueue(2);
+      queue.enqueue(3);
+      queue.enqueue(4);
+      expect(queue.dequeue(), 1);
+      expect(queue.dequeue(), 2);
+      expect(queue.dequeue(), 3);
+      expect(queue.dequeue(), 4);
+    });
+  });
+}
