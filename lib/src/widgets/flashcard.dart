49ff30b184bfd123ab6ad6de9f97ba86a2aa4b0f
Samuel Vargas <samvargasca@unal.edu.co>
2023-10-01 20:55:13 -0500

    Creación de animación para flashcards

--- lib/src/widgets/flashcard.dart
+++ lib/src/widgets/flashcard.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flip_card/flip_card.dart';
+import 'package:study_buddy/src/colors/colors.dart';
 
 class Flashcard extends StatefulWidget {
   const Flashcard(
@@ -29,29 +32,71 @@ class _FlashcardState extends State<Flashcard> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Card(
-        elevation: 4,
-        child: InkWell(
-          onTap: toggleCard,
-          child: Container(
-            width: widget.width,
-            height: widget.height,
-            alignment: Alignment.center,
-            padding: EdgeInsets.all(16),
-            child: isShowingAnswer
-                ? Text(widget.respuesta,
-                    style: const TextStyle(
-                        fontSize: 40,
-                        fontFamily: "Arimo",
-                        fontWeight: FontWeight.bold))
-                : Text(widget.pregunta,
-                    style: const TextStyle(
-                        fontSize: 40,
-                        fontFamily: "Arimo",
-                        fontWeight: FontWeight.bold)),
+      child: Container(
+        width: widget.width,
+        height: widget.height,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          border: Border.all(
+            color: azulRey,
+            width: 2,
+          ),
+        ),
+        child: FlipCard(
+          fill: Fill.fillBack,
+          direction: FlipDirection.HORIZONTAL,
+          side: CardSide.FRONT,
+          front: FittedBox(
+            fit: BoxFit.scaleDown,
+            child: AutoSizeText(widget.pregunta,
+                style: const TextStyle(
+                    fontSize: 40,
+                    fontFamily: "Arimo",
+                    fontWeight: FontWeight.bold)),
+          ),
+          back: FittedBox(
+            fit: BoxFit.scaleDown,
+            child: AutoSizeText(
+              widget.respuesta,
+              style: const TextStyle(
+                  fontSize: 40,
+                  fontFamily: "Arimo",
+                  fontWeight: FontWeight.bold),
+            ),
           ),
         ),
       ),
+      // child: Card(
+      //   elevation: 4,
+      //   child: InkWell(
+      //     onTap: toggleCard,
+      //     child: Container(
+      //       width: widget.width,
+      //       height: widget.height,
+      //       alignment: Alignment.center,
+      //       padding: EdgeInsets.all(16),
+      //       child: isShowingAnswer
+      // ? FittedBox(
+      //     fit: BoxFit.scaleDown,
+      //     child: AutoSizeText(
+      //       widget.respuesta,
+      //       style: const TextStyle(
+      //           fontSize: 40,
+      //           fontFamily: "Arimo",
+      //           fontWeight: FontWeight.bold),
+      //     ),
+      //   )
+      // : FittedBox(
+      //     fit: BoxFit.scaleDown,
+      //     child: AutoSizeText(widget.pregunta,
+      //         style: const TextStyle(
+      //             fontSize: 40,
+      //             fontFamily: "Arimo",
+      //             fontWeight: FontWeight.bold)),
+      //   ),
+      //     ),
+      //   ),
+      // ),
     );
   }
 }
