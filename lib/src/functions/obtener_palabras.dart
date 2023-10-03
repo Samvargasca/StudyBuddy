1e94596715b6da9f4ce73c87369482275589877c
Samuel Vargas <samvargasca@unal.edu.co>
2023-10-03 10:32:29 -0500

    Creación clase Palabra

---
+++ lib/src/functions/obtener_palabras.dart
@@ -0,0 +1,8 @@
+class Palabra {
+  Palabra(this.id, this.ingles, this.espanol, this.significado, this.ejemplo);
+  final int id;
+  final String ingles;
+  final String espanol;
+  final String significado;
+  final List<String> ejemplo;
+}
