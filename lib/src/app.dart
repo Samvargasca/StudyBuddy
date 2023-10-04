420cfe52ef88643e3cb0e56fa0c57c5e7b2387ba
Samuel Vargas <samvargasca@unal.edu.co>
2023-10-03 20:51:26 -0500

    Creación de rutas

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:study_buddy/src/pages/my_home_page.dart';
+import 'package:study_buddy/src/pages/traduccion/errores_page.dart';
+import 'package:study_buddy/src/pages/traduccion/traduccion_page.dart';
+import 'package:study_buddy/src/pages/traduccion/traduccion_ejercicio_page.dart';
+import 'package:study_buddy/src/pages/traduccion/palabra_page.dart';
+import 'package:study_buddy/src/pages/parejas_page.dart';
+import 'package:study_buddy/src/pages/parejas_game.dart';
 
 /// The Widget that configures your application.
 class MyApp extends StatelessWidget {
@@ -9,10 +15,26 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: "Study Buddy",
       debugShowCheckedModeBanner: false,
-      home: MyHomePage(title: "Home"),
+      initialRoute: "/", // Ruta inicial
+      routes: {
+        "/": (context) => const MyHomePage(
+            title: "Study Buddy"), // Ruta de la pagina principal
+        "/traduccion": (context) =>
+            const TraduccionPage(), // Ruta de la pagina de traduccion
+        "/traduccion/ejercicio": (context) =>
+            const TraduccionEjercicioPage(), // Ruta de la pagina de ejercicio de traduccion
+        "/traduccion/errores": (context) =>
+            const ErroresPage(), // Ruta de la pagina de errores de traduccion
+        "/traduccion/palabra": (context) =>
+            const PalabraPage(), // Ruta de la pagina de palabra individual
+        "/parejas": (context) =>
+            const ParejasPage(), // Ruta de la pagina de parejas
+        "/parejas/game": (context) =>
+            const ParejasGame(), // Ruta de la pagina de juego de parejas
+      },
     );
   }
 }
