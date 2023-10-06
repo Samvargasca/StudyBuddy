c0dc68c518ef6253f95c75170b803ef33c20834d
Samuel Vargas <samvargasca@unal.edu.co>
2023-10-06 16:05:15 -0500

    Nuevas funciones

--- lib/src/data_structures/linked_list.dart
+++ lib/src/data_structures/linked_list.dart
@@ -2,16 +2,24 @@
 
 class Node<T> {
   late T key;
-  Node? next;
+  Node<T>? next;
   Node(T value) {
     key = value;
   }
 }
 
 class LinkedList<T> {
-  Node? head, tail;
+  Node<T>? head, tail;
+
+  //Constructor
+  LinkedList() {
+    head = null;
+    tail = null;
+  }
 
   //Métodos
+
+  //Inserción de un dato
   void pushFront(T value) {
     Node<T> nuevo = Node<T>(value);
     nuevo.next = head;
@@ -20,20 +28,8 @@ class LinkedList<T> {
         head; //Si la cola estaba null, se coloca apuntando a la misma cabeza
   }
 
-  T popFront() {
-    if (head == null) {
-      throw Exception("La lista enlazada está vacía");
-    }
-    Node dev = head!;
-    head = head!.next;
-    if (head == null) {
-      tail == null;
-    }
-    return dev.key;
-  }
-
   void pushBack(T item) {
-    Node nuevo = Node(item);
+    Node<T> nuevo = Node(item);
     if (tail == null) {
       head = nuevo;
       tail = nuevo;
@@ -43,8 +39,22 @@ class LinkedList<T> {
     }
   }
 
+  // Eliminación de un dato al inicio o final
+
+  T popFront() {
+    if (head == null) {
+      throw Exception("La lista enlazada está vacía");
+    }
+    Node<T> dev = head!;
+    head = head!.next;
+    if (head == null) {
+      tail == null;
+    }
+    return dev.key;
+  }
+
   T popBack() {
-    late Node dev;
+    late Node<T> dev;
     if (head == null) {
       throw Exception("La lista enlazada está vacía");
     }
@@ -53,7 +63,7 @@ class LinkedList<T> {
       head = null;
       tail = null;
     } else {
-      Node p = head!;
+      Node<T> p = head!;
       while (p.next!.next != null) {
         p = p.next!;
       }
@@ -65,11 +75,78 @@ class LinkedList<T> {
   }
 
   void addAfter(node, key) {
-    Node node2 = Node(key);
+    Node<T> node2 = Node(key);
     node2.next = node.next;
     node.next = node2;
     if (tail == node) {
       tail == node2;
     }
+  }
+
+  // Actualización de un dato
+  void update(T oldData, T newData) {
+    if (head == null) throw Exception("La lista enlazada está vacía");
+
+    bool encontrado = false;
+
+    Node<T>? current = head!;
+    while (current != null) {
+      if (current.key == oldData) {
+        current.key = newData;
+        encontrado = true;
+        break;
+      }
+      current = current.next;
+    }
+
+    if (!encontrado) throw Exception("El dato no existe");
+  }
+
+  // Eliminación de un solo dato
+  void delete(T data) {
+    if (head == null) throw Exception("La lista enlazada está vacía");
+
+    if (head!.key == data) {
+      head = head!.next;
+      return;
+    }
+
+    bool encontrado = false;
+
+    Node<T>? current = head;
+    Node<T>? prev = head!;
+    while (current != null) {
+      if (current.key == data) {
+        prev!.next = current.next;
+        encontrado = true;
+        return;
+      }
+      prev = current;
+      current = current.next;
+    }
+    if (!encontrado) throw Exception("El dato no existe");
+  }
+
+  // Búsqueda de un solo dato
+  bool contains(T data) {
+    Node<T>? current = head;
+    while (current != null) {
+      if (current.key == data) {
+        return true;
+      }
+      current = current.next;
+    }
+    return false;
+  }
+
+  // Consulta de todos los datos
+  List<T> getAll() {
+    var current = head;
+    final dataList = <T>[];
+    while (current != null) {
+      dataList.add(current.key);
+      current = current.next;
+    }
+    return dataList;
   }
 }
