f012d21ae00b9bf784df021674ead019bf3945aa
Cristian Daniel <cmontanez@unal.edu.co>
2023-10-08 19:19:34 -0500

    Correcciones y restructuración de juego parejas

--- lib/src/pages/parejas/parejas_game.dart
+++ lib/src/pages/parejas/parejas_game.dart
@@ -10,11 +10,15 @@ class ParejasGame extends StatefulWidget {
 
 class _ParejasGame extends State<ParejasGame> {
   //!Variables
+  List<String> palabrasA = ["Hola", "Azul", "Gato", "Ave", "Negro"];
+  List<String> palabrasB = ["Blue", "Hello", "Bird", "Cat", "Black"];
+  String A = " ";
+  String B = " ";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //!Barra superior 
+      //!Barra superior
       appBar: AppBar(
         title: const Text(
           "StudyBuddy",
@@ -27,65 +31,99 @@ class _ParejasGame extends State<ParejasGame> {
         backgroundColor: azulClaro,
       ),
 
+
       //!Cuerpo
-      body: const Column(
+      body: Column(
         children: [
+          //!Barra superior con opcion de salida
+          const SizedBox(height: 25),
+            Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Container( height: 35, width: 100, decoration: BoxDecoration(borderRadius: BorderRadius.circular(20), color: azulOscuro),
+                child: const Center(
+                  child: Text( "Parejas", textAlign: TextAlign.center, style: TextStyle( fontFamily: "Arimo", fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white)),
+                ),
+              ),
+            const SizedBox(width: 105),
+              IconButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: const Icon( Icons.cancel, color: rojo, size: 37 ),
+              )
+            ],
+          ),
+          const SizedBox(height: 25),
+
           Expanded(
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-
               children: [
-                
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    CreaPalabras(palabra: 'hola'),
-                    SizedBox(height: 20),
-                    CreaPalabras(palabra: 'azul'),
-                    SizedBox(height: 20),
-                    CreaPalabras(palabra: 'Gato'),
-                    SizedBox(height: 20),
-                    CreaPalabras(palabra: 'Ave'),
-                    SizedBox(height: 20),
-                    CreaPalabras(palabra: 'negro'),
-                  ],
-                ),
+                Expanded(
+                  child: ListView.builder(
+                      itemCount: palabrasA.length,
+                      itemBuilder: (context, index) {
 
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    CreaPalabras(palabra: 'blue'),
-                    SizedBox(height: 20),
-                    CreaPalabras(palabra: 'hello'),
-                    SizedBox(height: 20),
-                    CreaPalabras(palabra: 'Bird'),
-                    SizedBox(height: 20),
-                    CreaPalabras(palabra: 'black'),
-                    SizedBox(height: 20),
-                    CreaPalabras(palabra: 'Cat'),
-                  ],
+                        return Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 7),
+                            child: CreaPalabras(
+                              palabra: palabrasA[index],
+                              onTap: () {
+                                setState(() {
+                                  A = palabrasA[index];
+                                  if (A == B) {
+                                    palabrasA.remove(A);
+                                  } else {
+                                  }
+                                });
+                              },
+                            ));
+                      }),
                 ),
-
+                const SizedBox(width: 35),
+                Expanded(
+                  child: ListView.builder(
+                      itemCount: palabrasB.length,
+                      itemBuilder: (context, index) {
+                        return Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 7),
+                            child: CreaPalabras(
+                              palabra: palabrasB[index],
+                              onTap: () {
+                                setState(() {
+                                  B = palabrasB[index];
+                                  if (A == B) {
+                                    palabrasB.remove(B);
+                                  }
+                                });
+                              },
+                            ));
+                      }),
+                ),
               ],
             ),
           ),
-          BarraInferior(),
+          const BarraInferior(),
         ],
       ),
     );
   }
 }
-
-
-class CreaPalabras extends StatelessWidget {
+class CreaPalabras extends StatefulWidget {
   final String palabra;
   final int? clave;
+  final Function onTap;
 
-  const CreaPalabras({
-    super.key, 
-    required this.palabra, 
-    this.clave
-    });
+  const CreaPalabras(
+      {super.key, required this.palabra, this.clave, required this.onTap});
+
+  @override
+  State<CreaPalabras> createState() => _CreaPalabras();
+}
+
+class _CreaPalabras extends State<CreaPalabras> {
+  Color colorButton = Colors.white;
 
   @override
   Widget build(BuildContext context) {
@@ -94,17 +132,19 @@ class CreaPalabras extends StatelessWidget {
       height: 100,
       child: ElevatedButton(
         onPressed: () {
-          
+          widget.onTap();
+          setState(() {
+            colorButton = Colors.blue;
+          });
         },
-
         style: ElevatedButton.styleFrom(
-          backgroundColor:Colors.white,
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          backgroundColor: colorButton,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
           side: const BorderSide(color: azulRey, width: 4),
         ),
-        
         child: Text(
-          palabra,
+          widget.palabra,
           style: const TextStyle(
               color: Colors.black,
               fontFamily: "Arimo",
@@ -115,3 +155,7 @@ class CreaPalabras extends StatelessWidget {
     );
   }
 }
+
+
+
+
