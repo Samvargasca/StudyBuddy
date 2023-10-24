ada5faaaf722acfa350fa4bc43b507d4d730584c
Samuel Vargas <samvargasca@unal.edu.co>
2023-10-24 10:23:14 -0500

    Muestra error en inicio de sesión

--- lib/src/pages/auth_management/login_view.dart
+++ lib/src/pages/auth_management/login_view.dart
@@ -18,76 +18,78 @@ class _LoginViewState extends State<LoginView> {
 
     return Scaffold(
       body: SafeArea(
-          child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            TextField(
-              decoration: const InputDecoration(labelText: "Email/User"),
-              controller: _emailController,
-            ),
-            TextField(
-              obscureText: true,
-              decoration: const InputDecoration(
-                labelText: "Password",
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              TextField(
+                decoration: const InputDecoration(labelText: "Email/User"),
+                controller: _emailController,
               ),
-              controller: _passwordController,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                ElevatedButton(
-                    onPressed: () {
+              TextField(
+                obscureText: true,
+                decoration: const InputDecoration(
+                  labelText: "Password",
+                ),
+                controller: _passwordController,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ElevatedButton(
+                    onPressed: () async {
                       try {
-                        firebaseService.signInWithEmailAndPassword(
+                        await firebaseService.signInWithEmailAndPassword(
                             _emailController.text, _passwordController.text);
                       } catch (e) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text(e.toString()),
+                          ),
+                        );
+                      }
+                    },
+                    child: const Text("Log In"),
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        try {
+                          firebaseService.createUserWithEmailAndPassword(
+                              _emailController.text, _passwordController.text);
+                        } catch (e) {
+                          if (context.mounted) {
+                            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                              content: Text(e.toString()),
+                              duration: const Duration(milliseconds: 300),
+                            ));
+                          }
+                        }
+                        print(_passwordController.text);
+                      },
+                      child: const Text("Sign Up"))
+                ],
+              ),
+              Text(firebaseService.user.toString()),
+              ElevatedButton(
+                  onPressed: () {
+                    try {
+                      firebaseService.signOut();
+                    } catch (e) {
+                      if (context.mounted) {
                         ScaffoldMessenger.of(context).showSnackBar(SnackBar(
                           content: Text(e.toString()),
                           duration: const Duration(milliseconds: 300),
                         ));
                       }
-                      print(_passwordController.text);
-                    },
-                    child: const Text("Log In")),
-                ElevatedButton(
-                    onPressed: () {
-                      try {
-                        firebaseService.createUserWithEmailAndPassword(
-                            _emailController.text, _passwordController.text);
-                      } catch (e) {
-                        if (context.mounted) {
-                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                            content: Text(e.toString()),
-                            duration: const Duration(milliseconds: 300),
-                          ));
-                        }
-                      }
-                      print(_passwordController.text);
-                    },
-                    child: const Text("Sign Up"))
-              ],
-            ),
-            Text(firebaseService.user.toString()),
-            ElevatedButton(
-                onPressed: () {
-                  try {
-                    firebaseService.signOut();
-                  } catch (e) {
-                    if (context.mounted) {
-                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                        content: Text(e.toString()),
-                        duration: const Duration(milliseconds: 300),
-                      ));
                     }
-                  }
-                  print(_passwordController.text);
-                },
-                child: const Text("Log Out"))
-          ],
+                    print(_passwordController.text);
+                  },
+                  child: const Text("Log Out"))
+            ],
+          ),
         ),
-      )),
+      ),
     );
   }
 }
