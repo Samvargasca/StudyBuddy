072b5c24f3abe1f96459ff8989edbec13e30d3cc
Samuel Vargas <samvargasca@unal.edu.co>
2023-11-02 19:31:34 -0500

    Mejora en formulario de login

--- lib/src/pages/auth_management/login_view.dart
+++ lib/src/pages/auth_management/login_view.dart
@@ -10,8 +10,6 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
-  final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
   bool validEmail = false;
   bool invisiblePassword = true;
@@ -42,53 +40,7 @@ class _LoginViewState extends State<LoginView> {
                   // Modificar con diseño correspondiente
                   "assets/icon/icon.png",
                 ),
-                Container(
-                  decoration: const BoxDecoration(
-                    color: Colors.white,
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(
-                        8.0), // Modificar con diseño correspondiente
-                    child: Column(
-                      children: [
-                        const Text(
-                            "Iniciar sesión"), // Modificar con diseño correspondiente
-                        const Text(
-                            "¡Hola, qué gusto verte de nuevo!"), // Modificar con diseño correspondiente
-                        FormularioInicioSesion(formKey: _formKey),
-                      ],
-                    ),
-                  ),
-                ),
-                ElevatedButton(
-                  // Modificar con diseño correspondiente
-                  onPressed: () async {
-                    currentContext = context;
-                    if (_formKey.currentState!.validate()) {
-                      var currentContext = context;
-                      try {
-                        await firebaseService.signInWithEmailAndPassword(
-                            _emailController.text, _passwordController.text);
-                        Navigator.pushNamed(currentContext, "/home");
-                      } catch (e) {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(
-                            content: Text(e.toString()),
-                          ),
-                        );
-                      }
-                    }
-                  },
-                  style: ElevatedButton.styleFrom(
-                    // Modificar con diseño correspondiente
-                    padding: const EdgeInsets.all(10),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    minimumSize: const Size(double.infinity, 0),
-                  ),
-                  child: const Text("Log In"),
-                ),
+                FormularioInicioSesion(formKey: _formKey),
                 Align(
                   alignment: Alignment.centerRight,
                   child: ElevatedButton(
@@ -112,21 +64,21 @@ class _LoginViewState extends State<LoginView> {
                       // },
                       child: const Text("Sign Up")),
                 ),
-                // ElevatedButton(
-                //     onPressed: () {
-                //       try {
-                //         firebaseService.signOut();
-                //       } catch (e) {
-                //         if (context.mounted) {
-                //           ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                //             content: Text(e.toString()),
-                //             duration: const Duration(milliseconds: 300),
-                //           ));
-                //         }
-                //       }
-                //       print(_passwordController.text);
-                //     },
-                //     child: const Text("Log Out"))
+                ElevatedButton(
+                  onPressed: () {
+                    try {
+                      firebaseService.signOut();
+                    } catch (e) {
+                      if (context.mounted) {
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                          content: Text(e.toString()),
+                          duration: const Duration(milliseconds: 300),
+                        ));
+                      }
+                    }
+                  },
+                  child: const Text("Log Out"),
+                ),
                 // This SizedBox helps to push items towards the center of the screen
                 SizedBox(height: MediaQuery.of(context).size.height * 0.1),
               ],
@@ -153,6 +105,7 @@ class _FormularioInicioSesionState extends State<FormularioInicioSesion> {
   bool _invisiblePassword = true;
   String _email = "";
   String _password = "";
+  var currentContext;
 
   @override
   Widget build(BuildContext context) {
@@ -162,48 +115,105 @@ class _FormularioInicioSesionState extends State<FormularioInicioSesion> {
       key: _formKey,
       child: Column(
         children: [
-          TextFormField(
-            keyboardType: TextInputType.emailAddress,
-            controller: _emailController,
-            onSaved: (newValue) => _email = newValue ?? "",
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return "Introduce texto";
-              }
-              if (!RegExp(
-                      r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
-                  .hasMatch(value)) {
-                return "Introduce un correo válido";
-              }
-              return null;
-            },
-            decoration: InputDecoration(
-              labelText: "Email",
-              suffixIcon: IconButton(
-                  onPressed: () => _emailController.clear(),
-                  icon: const Icon(Icons.close)),
+          Text(firebaseService.user?.email ?? "No hay usuario"),
+          Container(
+            decoration: const BoxDecoration(
+              color: Colors.white,
             ),
-          ),
-          TextFormField(
-            obscureText: _invisiblePassword,
-            onSaved: (newValue) => _password,
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return "Introduce una contraseña";
-              }
-              return null;
-            },
-            decoration: InputDecoration(
-              labelText: "Password",
-              suffixIcon: IconButton(
-                onPressed: () => setState(() {
-                  _invisiblePassword = !_invisiblePassword;
-                }),
-                icon: _invisiblePassword
-                    ? const Icon(Icons.visibility_off)
-                    : const Icon(Icons.visibility),
+            child: Padding(
+              padding: const EdgeInsets.all(
+                  8.0), // Modificar con diseño correspondiente
+              child: Column(
+                children: [
+                  const Text(
+                      "Iniciar sesión"), // Modificar con diseño correspondiente
+                  const Text(
+                      "¡Hola, qué gusto verte de nuevo!"), // Modificar con diseño correspondiente
+                  TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    controller: _emailController,
+                    onSaved: (newValue) => _email = newValue ?? "",
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Introduce texto";
+                      }
+                      if (!RegExp(
+                              r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                          .hasMatch(value)) {
+                        return "Introduce un correo válido";
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                      labelText: "Email",
+                      suffixIcon: IconButton(
+                          onPressed: () => _emailController.clear(),
+                          icon: const Icon(Icons.close)),
+                    ),
+                  ),
+                  TextFormField(
+                    obscureText: _invisiblePassword,
+                    onSaved: (newValue) => _password,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Introduce una contraseña";
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                      labelText: "Password",
+                      suffixIcon: IconButton(
+                        onPressed: () => setState(() {
+                          _invisiblePassword = !_invisiblePassword;
+                        }),
+                        icon: _invisiblePassword
+                            ? const Icon(Icons.visibility_off)
+                            : const Icon(Icons.visibility),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
+          ),
+          ElevatedButton(
+            // Modificar con diseño correspondiente
+            onPressed: () async {
+              if (_formKey!.currentState!.validate()) {
+                showDialog(
+                  context: context,
+                  barrierDismissible:
+                      false, // Impide cerrar la pantalla de carga con un tap afuera
+                  builder: (BuildContext context) {
+                    return const Center(
+                      child: CircularProgressIndicator(), // Pantalla de carga
+                    );
+                  },
+                );
+                // try {
+                await firebaseService.signInWithEmailAndPassword(
+                    _emailController.text, _passwordController.text);
+                Navigator.pushNamed(context, "/home");
+                // } catch (e) {
+                //   ScaffoldMessenger.of(context).showSnackBar(
+                //     SnackBar(
+                //       content: Text(e.toString()),
+                //     ),
+                //   );
+                // } finally {
+                Navigator.pop(context);
+                // }
+              }
+            },
+            style: ElevatedButton.styleFrom(
+              // Modificar con diseño correspondiente
+              padding: const EdgeInsets.all(10),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              minimumSize: const Size(double.infinity, 0),
+            ),
+            child: const Text("Log In"),
           ),
         ],
       ),
