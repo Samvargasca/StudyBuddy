c8977a15db56b2f576ad2a7d4d8a711d4bf6289a
Samuel Vargas <samvargasca@unal.edu.co>
2023-11-05 13:45:16 -0500

    Subida de clase palabra

--- lib/src/services/firestore_service.dart
+++ lib/src/services/firestore_service.dart
@@ -1,5 +1,8 @@
 import "package:cloud_firestore/cloud_firestore.dart";
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'dart:convert';
 
 class FirestoreService extends ChangeNotifier {
   final FirebaseFirestore _db = FirebaseFirestore.instance;
@@ -39,5 +42,96 @@ class FirestoreService extends ChangeNotifier {
     } catch (e) {
       rethrow;
     }
+  }
+
+  // Referencia a la coleccion de palabras
+  late final CollectionReference _palabrasCollectionRef =
+      _db.collection('palabras').withConverter(
+            fromFirestore: Palabra.fromFirestore,
+            toFirestore: (Object? palabra, _) {
+              if (palabra is Palabra) {
+                return palabra.toFirestore();
+              }
+              throw Exception("Se está intentando cargar un tipo diferente");
+            },
+          );
+
+  Future<void> createPalabra(Palabra palabra) async {
+    await _palabrasCollectionRef.add(palabra);
+  }
+
+  Future<List<Palabra>> cargarPalabrasFromJson() async {
+    String jsonString =
+        await rootBundle.loadString('assets/json/datos_prueba.json');
+
+    List<dynamic> jsonData = json.decode(jsonString);
+    List<Palabra> palabras = jsonData.map((e) => Palabra.fromJson(e)).toList();
+    return palabras;
+  }
+}
+
+class Palabra {
+  String ingles;
+  String espanol;
+  List<String> ejemplos;
+  String definicion;
+
+  Palabra(this.espanol, this.ingles, this.definicion, this.ejemplos);
+
+  factory Palabra.fromFirestore(DocumentSnapshot<Map<String, dynamic>> snapshot,
+      SnapshotOptions? options) {
+    final data = snapshot.data();
+    return Palabra(
+      data?['espanol'],
+      data?['ingles'],
+      data?['definicion'],
+      data?['ejemplos'] is Iterable
+          ? List.from(data?['ejemplos'])
+          : throw Exception("Fallo al obtener los ejemplos"),
+    );
+  }
+
+  Map<String, dynamic> toFirestore() {
+    return {
+      'espanol': espanol,
+      'ingles': ingles,
+      'definicion': definicion,
+      'ejemplos': ejemplos,
+    };
+  }
+
+  // Función para comparar listas por contenido
+  bool listEquals(List a, List b) {
+    if (identical(a, b)) return true;
+    if (a.length != b.length) return false;
+    for (int i = 0; i < a.length; i++) {
+      if (a[i] != b[i]) return false;
+    }
+    return true;
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      other is Palabra &&
+      other.ingles == ingles &&
+      other.espanol == espanol &&
+      listEquals(other.ejemplos,
+          ejemplos) && // Utiliza una función de comparación de listas
+      other.definicion == definicion;
+
+  @override
+  int get hashCode =>
+      ingles.hashCode ^
+      espanol.hashCode ^
+      ejemplos.hashCode ^
+      definicion.hashCode;
+
+  factory Palabra.fromJson(Map<String, dynamic> json) {
+    return Palabra(
+      json['espanol'] as String,
+      json['ingles'] as String,
+      json['definicion'] as String,
+      (json['ejemplos'] as List<dynamic>).cast<String>(),
+    ); // Ensure ejemplos is a List<String>
   }
 }
