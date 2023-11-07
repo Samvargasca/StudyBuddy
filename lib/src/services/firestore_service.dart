b46d44181f75d077ac3d52c27abd70339f93e7ab
Samuel Vargas <samvargasca@unal.edu.co>
2023-11-06 21:32:40 -0500

    Creación de usuario

--- lib/src/services/firestore_service.dart
+++ lib/src/services/firestore_service.dart
@@ -214,3 +214,67 @@ class Palabra {
     ); // Ensure ejemplos is a List<String>
   }
 }
+
+class Usuario {
+  String id;
+  String usuario;
+  List<String> errores;
+  int tiempoTraduccion;
+  int tiempoParejas;
+
+  Usuario(this.id, this.usuario, this.errores, this.tiempoTraduccion,
+      this.tiempoParejas);
+
+  factory Usuario.fromFirestore(
+      QueryDocumentSnapshot<Object?> snapshot, SnapshotOptions? options) {
+    if (snapshot is QueryDocumentSnapshot<Map<String, dynamic>>) {
+      final data = snapshot.data();
+
+      return Usuario(
+        snapshot.id,
+        data['usuario'],
+        data['errores'] is Iterable
+            ? List.from(data['errores'])
+            : throw Exception("Fallo al obtener los errores"),
+        data['tiempoTraduccion'],
+        data['tiempoParejas'],
+      );
+    }
+    throw Exception("Se están cargando valores erróneos");
+  }
+
+  Map<String, dynamic> toFirestore() {
+    return {
+      'usuario': usuario,
+      'errores': errores,
+      'tiempoTraduccion': tiempoTraduccion,
+    };
+  }
+
+  // Función para comparar listas por contenido
+  bool listEquals(List a, List b) {
+    if (identical(a, b)) return true;
+    if (a.length != b.length) return false;
+    for (int i = 0; i < a.length; i++) {
+      if (a[i] != b[i]) return false;
+    }
+    return true;
+  }
+
+  // función para comparar dos usuarios
+  @override
+  bool operator ==(Object other) =>
+      other is Usuario &&
+      other.id == id &&
+      other.usuario == usuario &&
+      listEquals(other.errores,
+          errores) && // Utiliza una función de comparación de listas
+      other.tiempoTraduccion == tiempoTraduccion;
+
+  @override
+  int get hashCode =>
+      id.hashCode ^
+      usuario.hashCode ^
+      errores.hashCode ^
+      tiempoTraduccion.hashCode;
+}
