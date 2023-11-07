4295b0e30736ff5e10393dc7cb8791ccf9e8fcfc
Cristian Daniel <cmontanez@unal.edu.co>
2023-11-07 08:54:15 -0500

    Base de datos en juego de parejas

--- lib/src/widgets/flashcard.dart
+++ lib/src/widgets/flashcard.dart
@@ -17,6 +17,7 @@ class Flashcard extends StatefulWidget {
   final double height;
 
   @override
+  // ignore: library_private_types_in_public_api
   _FlashcardState createState() => _FlashcardState();
 }
 
