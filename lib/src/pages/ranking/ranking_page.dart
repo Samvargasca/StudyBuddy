5df693aab7b85dc55dd9160daf83867f411c55f5
Samuel Vargas <samvargasca@unal.edu.co>
2023-11-06 23:03:24 -0500

    Implementación de rankings ordenados usando bst

--- lib/src/pages/ranking/ranking_page.dart
+++ lib/src/pages/ranking/ranking_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:study_buddy/src/data_structures/bst_usuario.dart';
 import 'package:study_buddy/src/widgets/barra_inferior.dart';
 import 'package:study_buddy/src/constants/colors.dart';
 import 'package:study_buddy/src/services/firestore_service.dart';
@@ -23,7 +24,25 @@ class _RankingPageState extends State<RankingPage> {
   Future<List<Usuario>> obtenerUsuarios() async {
     FirestoreService firestoreService =
         Provider.of<FirestoreService>(context, listen: false);
-    return await firestoreService.obtenerUsuarios();
+    List<Usuario> usuarios = await firestoreService.obtenerUsuarios();
+    if (botonPresionado == "traduccion") {
+      BSTUsuarioTraduccion bstUsuarioTraduccion = BSTUsuarioTraduccion();
+      for (Usuario usuario in usuarios) {
+        if (usuario.tiempoTraduccion != null) {
+          bstUsuarioTraduccion.insert(usuario);
+        }
+      }
+      return bstUsuarioTraduccion.inorder(bstUsuarioTraduccion.root);
+    } else if (botonPresionado == "parejas") {
+      BSTUsuarioParejas bstUsuarioParejas = BSTUsuarioParejas();
+      for (Usuario usuario in usuarios) {
+        if (usuario.tiempoParejas != null) {
+          bstUsuarioParejas.insert(usuario);
+        }
+      }
+      return bstUsuarioParejas.inorder(bstUsuarioParejas.root);
+    }
+    return usuarios;
   }
 
   @override
@@ -128,127 +147,137 @@ class _RankingPageState extends State<RankingPage> {
                     } else {
                       List<Usuario> usuarios = snapshot.data as List<Usuario>;
                       return Expanded(
-                        child: ListView.builder(
-                          itemBuilder: (context, index) {
-                            Widget leadingWidget;
-                            // Si el usuario es el primero, se le pone un borde amarillo
-                            if (index == 0) {
-                              leadingWidget = Container(
-                                decoration: const BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: dorado,
-                                  border: Border.fromBorderSide(
-                                    BorderSide(
-                                      color: doradoBorde,
-                                      width: 4,
-                                    ),
-                                  ),
+                        child: usuarios.isEmpty
+                            ? const Text(
+                                "No hay usuarios registrados",
+                                style: TextStyle(
+                                  fontFamily: "Arimo",
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20,
                                 ),
-                                width: 50,
-                                height: 50,
-                                child: Center(
-                                  child: Text(
-                                    "${index + 1}",
-                                    style: const TextStyle(
-                                      fontFamily: "Arimo",
-                                      fontSize: 25,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white,
-                                    ),
-                                  ),
-                                ),
-                              );
-                            } else if (index == 1) {
-                              // Si el usuario es el segundo, se le pone un borde plateado
-                              leadingWidget = Container(
-                                decoration: const BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: plateado,
-                                  border: Border.fromBorderSide(
-                                    BorderSide(
-                                      color: plateadoBorde,
-                                      width: 4,
-                                    ),
-                                  ),
-                                ),
-                                width: 50,
-                                height: 50,
-                                child: Center(
-                                  child: Text(
-                                    "${index + 1}",
-                                    style: const TextStyle(
-                                      fontFamily: "Arimo",
-                                      fontSize: 25,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white,
-                                    ),
-                                  ),
-                                ),
-                              );
-                            } else if (index == 2) {
-                              // Si el usuario es el tercero, se le pone un borde bronce
-                              leadingWidget = Container(
-                                decoration: const BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: bronce,
-                                  border: Border.fromBorderSide(
-                                    BorderSide(
-                                      color: bronceBorde,
-                                      width: 4,
-                                    ),
-                                  ),
-                                ),
-                                width: 50,
-                                height: 50,
-                                child: Center(
-                                  child: Text(
-                                    "${index + 1}",
-                                    style: const TextStyle(
-                                      fontFamily: "Arimo",
-                                      fontSize: 25,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white,
-                                    ),
-                                  ),
-                                ),
-                              );
-                            } else {
-                              // Si el usuario no es ninguno de los anteriores, se le pone un borde azul
-                              leadingWidget = Container(
-                                decoration: const BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  border: Border.fromBorderSide(
-                                    BorderSide(
-                                      color: azulRey,
-                                      width: 4,
-                                    ),
-                                  ),
-                                ),
-                                width: 50,
-                                height: 50,
-                                child: Center(
-                                  child: Text(
-                                    "${index + 1}",
-                                    style: const TextStyle(
-                                      fontFamily: "Arimo",
-                                      fontSize: 25,
-                                      fontWeight: FontWeight.bold,
-                                      color: azulRey,
-                                    ),
-                                  ),
-                                ),
-                              );
-                            }
-                            return ListTile(
-                              leading: leadingWidget,
-                              title: Text(usuarios[index].usuario),
-                              trailing: Text(botonPresionado == "traduccion"
-                                  ? "${usuarios[index].tiempoTraduccion} s"
-                                  : "${usuarios[index].tiempoParejas} s"),
-                            );
-                          },
-                          itemCount: usuarios.length,
-                        ),
+                              )
+                            : ListView.builder(
+                                itemBuilder: (context, index) {
+                                  Widget leadingWidget;
+                                  // Si el usuario es el primero, se le pone un borde amarillo
+                                  if (index == 0) {
+                                    leadingWidget = Container(
+                                      decoration: const BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        color: dorado,
+                                        border: Border.fromBorderSide(
+                                          BorderSide(
+                                            color: doradoBorde,
+                                            width: 4,
+                                          ),
+                                        ),
+                                      ),
+                                      width: 50,
+                                      height: 50,
+                                      child: Center(
+                                        child: Text(
+                                          "${index + 1}",
+                                          style: const TextStyle(
+                                            fontFamily: "Arimo",
+                                            fontSize: 25,
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ),
+                                    );
+                                  } else if (index == 1) {
+                                    // Si el usuario es el segundo, se le pone un borde plateado
+                                    leadingWidget = Container(
+                                      decoration: const BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        color: plateado,
+                                        border: Border.fromBorderSide(
+                                          BorderSide(
+                                            color: plateadoBorde,
+                                            width: 4,
+                                          ),
+                                        ),
+                                      ),
+                                      width: 50,
+                                      height: 50,
+                                      child: Center(
+                                        child: Text(
+                                          "${index + 1}",
+                                          style: const TextStyle(
+                                            fontFamily: "Arimo",
+                                            fontSize: 25,
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ),
+                                    );
+                                  } else if (index == 2) {
+                                    // Si el usuario es el tercero, se le pone un borde bronce
+                                    leadingWidget = Container(
+                                      decoration: const BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        color: bronce,
+                                        border: Border.fromBorderSide(
+                                          BorderSide(
+                                            color: bronceBorde,
+                                            width: 4,
+                                          ),
+                                        ),
+                                      ),
+                                      width: 50,
+                                      height: 50,
+                                      child: Center(
+                                        child: Text(
+                                          "${index + 1}",
+                                          style: const TextStyle(
+                                            fontFamily: "Arimo",
+                                            fontSize: 25,
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ),
+                                    );
+                                  } else {
+                                    // Si el usuario no es ninguno de los anteriores, se le pone un borde azul
+                                    leadingWidget = Container(
+                                      decoration: const BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        border: Border.fromBorderSide(
+                                          BorderSide(
+                                            color: azulRey,
+                                            width: 4,
+                                          ),
+                                        ),
+                                      ),
+                                      width: 50,
+                                      height: 50,
+                                      child: Center(
+                                        child: Text(
+                                          "${index + 1}",
+                                          style: const TextStyle(
+                                            fontFamily: "Arimo",
+                                            fontSize: 25,
+                                            fontWeight: FontWeight.bold,
+                                            color: azulRey,
+                                          ),
+                                        ),
+                                      ),
+                                    );
+                                  }
+                                  return ListTile(
+                                    leading: leadingWidget,
+                                    title: Text(usuarios[index].usuario),
+                                    trailing: Text(botonPresionado ==
+                                            "traduccion"
+                                        ? "${usuarios[index].tiempoTraduccion} s"
+                                        : "${usuarios[index].tiempoParejas} s"),
+                                  );
+                                },
+                                itemCount: usuarios.length,
+                              ),
                       );
                     }
                   }),
